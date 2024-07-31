35376c0f47fddc8e799e3f680820c5cd53da4e4c
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-07-31 20:44:31 +0530

    added nav

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    
+    <div className='logo'>
+         
+        <img src="https://raw.githubusercontent.com/anshuopinion/React-10-Projects/project-1/project-1/public/images/brand_logo.png" alt="" />
+        
+        <ul>
+        <span>MENU</span>
+         <span>LOCATION</span>
+         <span>ABOUT</span>
+         <span>CONTACT</span>
+        </ul>
+        
+        <button className='login'>Login</button>
+        
+    
+    </div>
+  )
+}
+
+export default App
